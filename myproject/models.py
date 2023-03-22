ecf7164556e69aaebfdc23a0fadd2af70df006d0
Admin+1-at-331008949036 <monika_sahay@yahoo.com>
2023-03-22 16:17:35 +0000

    added user class

--- myproject/models.py
+++ myproject/models.py
@@ -1,5 +1,14 @@
 # setup db inside the __init__.py under myproject folders
-from myproject import db
+from myproject import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
 
 
 class Puppy(db.Model):
