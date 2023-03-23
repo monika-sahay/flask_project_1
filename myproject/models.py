4658380548f72093a96eb7a6ed8e79987f53a665
Admin+1-at-331008949036 <monika_sahay@yahoo.com>
2023-03-23 12:20:37 +0000

    added users folder and forms for the users

--- myproject/models.py
+++ myproject/models.py
@@ -3,12 +3,26 @@ from myproject import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 class User(db.model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(64))
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Puppy(db.Model):
@@ -21,6 +35,7 @@ class Puppy(db.Model):
 
     def __init__(self,name):
         self.name = name
+
 
     def __repr__(self):
 
---
+++ myproject/users/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Log in")
+
+class RegistraForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),EqualTo(pass_confirm), message='passwords must match'])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired])
+    submit = SubmitField('Register!')
+
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email already registered')
+            
+    
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username taken')
+
+
+
